955bbf3e767a7814f563de447ca6487dd31a4d61
Ahmed Mawari <afmawari@umich.edu>
2020-11-19 22:15:37 -0500

    Added JUnit test method "testAddCc", to test addCc method from Email class

--- src/test/java/org/apache/commons/mail/EmailTest.java
+++ src/test/java/org/apache/commons/mail/EmailTest.java
@@ -1,6 +1,41 @@
 package org.apache.commons.mail;
 
-public class EmailTest {
-	
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
+
+public class EmailTest {
+	private static final String[] TEST_EMAILS = { "test@test.com", "a.sas@so.com", "some@as.org" , "sdqwqdwfq@asdsadad.com.uk"};
+	// Concrete email class for testing  
+	private EmailConcrete email;
+	
+	@Before 
+	public void setUpEmailTest() throws Exception {
+		email = new EmailConcrete();		
+	}
+	@After
+	public void tearDownEmailTest() throws Exception{
+		email= null;
+		assertNull(email);
+	}
+	
+	@Test
+	public void testAddBcc() throws Exception{
+		email.addBcc(TEST_EMAILS); // adding premade valid emails list to bcc 
+		assertEquals(4, email.getBccAddresses().size()); // asserting that the number of emails added to email bcc list is equal 
+		String[] testDummy = {}; // empty string list to test null/invalid list
+		try {
+			email.addBcc(testDummy); // sending test list to addbcc method
+		}	
+		catch (IllegalArgumentException e) {
+			assertEquals("Address List provided was invalid", e.getMessage()); // asserting that the exception message is the same 
+		}
+
+	}
+
+	
 }
